6399be11080163285ef9eeaa3e7f2238eb7b5649
pbutsik <p.butsik@gmail.com>
2023-10-12 10:26:54 +0300

    review_2

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,24 @@
 const page = document.querySelector(".page");
 const cardContainer = page.querySelector(".places__list");
+const cardTemplate = document.querySelector("#card-template").content;
 
-function deleteFunction() {
-  const deleteButton = document.querySelector(".card__delete-button");
-  const card = deleteButton.closest(".card");
+function updateCardInitDict(cardInit) {
+  // принимает на вход массив обьектов
+  // добавляет в обьект alt и возвращает обновленный массив обьектов
+  const updatedInitialCards = cardInit.map(function (item) {
+    item["alt"] = `фото места - ${item["name"]}`;
 
-  card.remove();
+    return item;
+  });
+
+  return updatedInitialCards;
 }
 
-function addCard(card, delFunc) {
-  const cardTemplate = document.querySelector("#card-template").content;
+function createCard(card, delFunc) {
+  // создание и наполнение карточки
   const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
 
+  cardElement.querySelector(".card__image").alt = card["alt"];
   cardElement.querySelector(".card__image").src = card["link"];
   cardElement.querySelector(".card__title").textContent = card["name"];
   cardElement
@@ -19,15 +26,33 @@ function addCard(card, delFunc) {
     .addEventListener("click", function (evt) {
       evt.target.classList.toggle("card__like-button_is-active");
     });
-
   cardElement
     .querySelector(".card__delete-button")
-    .addEventListener("click", delFunc);
+    .addEventListener("click", function (evt) {
+      const eventTargetButton = evt.target;
+      delFunc(eventTargetButton);
+    });
 
+  return cardElement;
+}
+
+function deleteCard(btn = null) {
+  // удаление карточки по полученной кнопке
+  const card = btn.closest(".card");
+  card.remove();
+}
+
+function addCard(...args) {
+  // добавляет в DOM карточку
+  cardElement = createCard(args[0], args[1]);
   cardContainer.append(cardElement);
 }
 
-for (let item of initialCards) {
-  console.log(item["name"], item["link"]);
-  addCard(item, deleteFunction);
+function main() {
+  // основная функция запуска скриптов
+  for (let item of updateCardInitDict(initialCards)) {
+    addCard(item, deleteCard);
+  }
 }
+
+main();
